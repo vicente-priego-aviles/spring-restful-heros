1b86e88d4e6057e743ba4f827f9fd17e60990873
vicente-priego-aviles <vicente.priego.aviles@gmail.com>
2023-05-19 21:39:06 +0200

    solucionado bug

--- src/main/java/com/javacadabra/hero/LoadData.java
+++ src/main/java/com/javacadabra/hero/LoadData.java
@@ -2,8 +2,6 @@ package com.javacadabra.hero;
 
 import com.github.javafaker.Faker;
 import com.github.javafaker.Superhero;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,24 +11,19 @@ import java.util.stream.IntStream;
 @Configuration
 class LoadData {
 
-    private static final Logger log = LoggerFactory.getLogger(LoadData.class);
-
     @Bean
     CommandLineRunner iniciarDatos(HeroRepository repository) {
 
-        return args -> {
-            IntStream.range(0, 100).forEach(i ->
-                {
-                    Superhero hero = new Faker().superhero();
+        return args -> IntStream.range(0, 100).forEach(i -> {
+            Superhero hero = new Faker().superhero();
 
-                    repository.save(
-                            Hero.builder()
-                                    .name(hero.name())
-                                    .power(hero.power())
-                                    .descriptor(hero.descriptor())
-                                    .build()
-                    );
-                });
-        };
+            repository.save(
+                    Hero.builder()
+                            .name(hero.name())
+                            .power(hero.power())
+                            .descriptor(hero.descriptor())
+                            .build()
+            );
+        });
     }
 }
